4f0d5edeca67207b0137c5848d6150ef8caebb0a
SobitThapa <sobit.thapa@student.pu.edu.np>
2025-06-16 20:57:29 +0545

    ...

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react(),
   visualizer({ open: true }),],
   assetsInclude: ['**/*.glb'],
+  base: '/3D_portfolio/',
   build: {
     rollupOptions: {
       output: {
