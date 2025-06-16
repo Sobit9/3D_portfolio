f5a2181aefdc8c8b73bb842301e0d2e184e46224
SobitThapa <sobit.thapa@student.pu.edu.np>
2025-06-16 21:08:58 +0545

    ....

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(),
-  visualizer({ open: true }),],
+  plugins: [react(),],
   assetsInclude: ['**/*.glb'],
-  base: '/3D_portfolio/',
+  // base: '/3D_portfolio/',
   build: {
     rollupOptions: {
       output: {
