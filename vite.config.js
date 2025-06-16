c7670670f28d608974a6be4df20d9df2db689a37
Sobit9 <59721182+Sobit9@users.noreply.github.com>
2025-06-16 22:16:54 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(),],
   assetsInclude: ['**/*.glb'],
-  // base: '/3D_portfolio/',
+  base: '/3D_portfolio/',
   build: {
     rollupOptions: {
       output: {
